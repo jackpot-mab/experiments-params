f0cd8e69ed07aeba1b8ffb19935c7e82ded5ffdc
Luciana123 <13137311+Luciana123@users.noreply.github.com>
2023-09-10 21:48:57 -0300

    experiment params

--- model/experiment.go
+++ model/experiment.go
@@ -1,14 +1,14 @@
 package model
 
 type Experiment struct {
-	ExperimentId string
-	PolicyType   string
-	Arms         []Arm
-	Parameters   interface{}
+	ExperimentId string      `json:"experiment_id"`
+	PolicyType   string      `json:"policy_type"`
+	Arms         []Arm       `json:"arms"`
+	Parameters   interface{} `json:"parameters"`
 }
 
 type Arm struct {
-	Name string
+	Name string `json:"name"`
 }
 
 type EpsilonGreedyParams struct {
